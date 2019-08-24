7198066e7ed1db9a79708b1552d0bce79339a4dc
sam sepio <samsepio66sqgmail.com>
2019-08-24 18:02:35 +0000

    listo listo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app=express();
 
 app.set('puerto','8000');
 
-app.use(express.static(path.join(__dirname,'frontend')));
+app.use(express.static(path.join(__dirname,'public')));
 
 const server=app.listen(app.get('puerto'),()=>{
 	console.log('servidor ejecutandose en el puerto',app.get('puerto'));
